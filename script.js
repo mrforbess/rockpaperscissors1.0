c16d4ac1ffabc8dc517c2e848aa769bfa55395ee
Kwasi <kwasiokrah0@gmail.com>
2022-10-26 13:04:40 +0000

    index.html

--- script.js
+++ script.js
@@ -1,11 +1,69 @@
-//var userChoice = prompt("Rock, Paper, Scissors?")
-var computerChoice = Math.floor(Math.random()*4); // math floor will round it to the nearest whole number and  multiply by 3 allows code to generate random number between 0 and 3
-if (computerChoice == 1){
-    computerChoice ="Rock";
-}else if(computerChoice == 2){
-    computerChoice = "Paper";
-}else{
-    computerChoice = "Scissors";
-}
-console.log(computerChoice);
+let playerScore=0;
+let computerScore=0;
+let tieScore=0;
 
+function getComputerChoice(){
+let rps = ["rock", "paper", "scissors"];
+let random = rps[Math.floor(Math.random() * rps.length)];
+return random;
+}
+
+
+function playRound(playerSelection, computerSelection ){
+    if (playerSelection == computerSelection){
+        tieScore++ // we are telling the code to keep count and increase tie score by 1, each time this happens
+        console.log("It's a draw");
+    }else if ((playerSelection.toLowerCase() === "rock") && (computerSelection === "scissors")) {
+        playerScore++
+        return (" You played rock. You win! Rock crushes scissors");
+      } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+        computerScore++
+        return ("You played rock. You lose! Paper covers rock");
+      } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+        playerScore++
+        return ("You played paper. You win! Paper covers rock");
+      } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+        computerScore++
+        return ("You played paper. You lose! Scissors cuts paper");
+      } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+        playerScore++
+        return ("You played scissors. You win! Scissors cuts paper");
+      } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+        computerScore++
+        return ("You played scissors. You lose!Rock crushes scissors");
+      }else{
+        error = "You must input rock,paper or scissors!"
+        return (error)
+      }
+}
+//const playerSelection = "rock";   //this piece of code was the first run inside the console before we made the user input their choice at the end
+
+//console.log(playRound(playerSelection, computerSelection));
+
+
+
+function game() {
+    for (let i = 1; i < 6; i++) {
+    const playerSelection = prompt(" Game " + i +". Choose your weapon").toLowerCase() // because we want a selection each time
+    const computerSelection = getComputerChoice(); // do this so you can get a new selection each time
+    const computerPlay = ("Computer played " + computerSelection+ ".") // logs the computer's selection
+    alert(computerPlay + " " + playRound(playerSelection, computerSelection))
+    }
+    
+
+    if (playerScore > computerScore){
+    alert("WINNER!! Play again?");
+    } else if(computerScore > playerScore){;
+    alert("You failed this time! Try again?")
+    } else{
+    alert("You tied with the computer!")
+    }
+}
+
+console.log(game())
+
+
+
+/*let computerSelection = getComputerChoice();
+let playerSelection = prompt("Choose your weapon");
+console.log(playRound(playerSelection, computerSelection)); */
